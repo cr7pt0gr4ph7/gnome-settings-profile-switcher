ea84aa4f2cccb3e5c95dd5b021dce8f1f062cbca
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2024-04-18 12:16:11 +0200

    Make it run on GNOME 44.

--- extension.js
+++ extension.js
@@ -1,34 +1,86 @@
-/* extension.js
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * SPDX-License-Identifier: GPL-2.0-or-later
- */
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+// GNOME >= 45 uses ESModules
+import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
+import * as QuickSettings from 'resource:///org/gnome/shell/ui/quickSettings.js';
+*/
 
-/* exported init */
+// GNOME <= 44 uses a custom import system
+const {Gio, GObject} = imports.gi;
+const Main = imports.ui.main;
+const PanelMenu = imports.ui.panelMenu;
+const PopupMenu = imports.ui.popupMenu;
+const QuickSettings = imports.ui.quickSettings;
+const ExtensionUtils = imports.misc.extensionUtils;
+const Me = ExtensionUtils.getCurrentExtension();
 
-class Extension {
+const ProfileSwitcherIndicator = GObject.registerClass(
+    class ProfileSwitcherIndicator extends QuickSettings.SystemIndicator {
+        _init(extensionObject) {
+            super._init();
+
+            // Create an icon for the indicator
+            this._indicator = this._addIndicator();
+            this._indicator.icon_name = 'selection-mode-symbolic';
+
+            // Showing an indicator when the feature is enabled
+            // GNOME 45+
+            // this._settings = extensionObject.getSettings();
+            // GNOME 44
+            this._settings = ExtensionUtils.getSettings();
+            this._settings.bind('feature-enabled',
+                this._indicator, 'visible',
+                Gio.SettingsBindFlags.DEFAULT);
+        }
+    });
+
+const ExampleToggle = GObject.registerClass(
+    class ExampleToggle extends QuickSettings.QuickToggle {
+        _init(extensionObject) {
+            super._init({
+                title: _('Example Title'),
+                subtitle: _('Example Subtitle'),
+                iconName: 'selection-mode-symbolic',
+                toggleMode: true,
+            });
+
+            // Binding the toggle to a GSettings key
+            // GNOME 45+
+            // this._settings = extensionObject.getSettings();
+            // GNOME 44
+            this._settings = ExtensionUtils.getSettings();
+            this._settings.bind('feature-enabled',
+                this, 'checked',
+                Gio.SettingsBindFlags.DEFAULT);
+        }
+    });
+
+class TunedProfileSwitcher {
     constructor() {
     }
 
     enable() {
+        this._indicator = new ProfileSwitcherIndicator(this);
+        this._indicator.quickSettingsItems.push(new ExampleToggle(this));
+
+        // GNOME 45
+        // Main.panel.statusArea.quickSettings.menu._indicators.add_child(this);
+        Main.panel.statusArea.quickSettings._addItems(this._indicator.quickSettingsItems);
+
+        // GNOME 45+
+        // Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
     }
 
     disable() {
+        this._indicator.quickSettingsItems.forEach(item => item.destroy());
+        this._indicator.destroy();
+        this._indicator = null;
     }
 }
 
 function init() {
-    return new Extension();
+    return new TunedProfileSwitcher();
 }
